b5cc7161c954ee1b362b8171adc1b202360b3f69
Sam Brown <sbr@activeviam.com>
2018-06-27 10:47:37 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # impala-jdbc-example
+
+This is a basic quick and dirty class to connect to the Impala using Cloudera's Impala JDBC driver
+We connect to the quickstart.cloudera VM which is running locally in VirtualBox.
+
+You will need to install and put the ImpalaJDBC41 driver on the classpath. This can be downloaded from: https://www.cloudera.com/downloads/connectors/impala/jdbc/2-6-3.html
+
+Test with CDH quickstart 5.13 and Impala JDBC driver 2.6.3
---
+++ src/test/java/com/sbrown52/ImpalaTest.java
@@ -0,0 +1,46 @@
+package com.sbrown52;
+
+/*
+This is a basic quick and dirty class to connect to the Impala using Cloudera's Impala JDBC driver
+We connect to the quickstart.cloudera VM which is running locally in VirtualBox.
+We are using version 5.13
+*/
+
+import java.sql.*;
+
+public class ImpalaTest {
+    // JDBC DRIVER classname
+    public static final String JDBC_DRIVER = "com.cloudera.impala.jdbc41.Driver";
+    // String of impala connection
+    public static final String CONNETION_URL = "jdbc:impala://quickstart.cloudera:21050";
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        // SQL query to run against Impala, using data loading in CDH tutorial
+        String impalaQuery = "select * from products limit 10";
+        try {
+            Class.forName(JDBC_DRIVER);
+            con = DriverManager.getConnection(CONNETION_URL);
+
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(impalaQuery);
+
+            // Print out some results
+            while (rs.next()) {
+                System.out.printf(rs.getString(1) + ", ");
+                System.out.printf(rs.getString(2)+ ", ");
+                System.out.printf(rs.getString(2)+ ", ");
+                System.out.printf(rs.getString(3)+ ", ");
+                System.out.printf(rs.getString(4)+ ", ");
+                System.out.printf(rs.getString(5)+ ", ");
+                System.out.println("");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // should probably close connection
+
+    }
+}
